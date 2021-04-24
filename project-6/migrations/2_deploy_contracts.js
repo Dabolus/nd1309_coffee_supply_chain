c20d9bc5c9b01eb1139ee185b159cbe9a39e1b87
Giorgio Garasto <giorgio@garasto.me>
2021-04-24 18:08:38 +0200

    style: code formatting

--- project-6/migrations/2_deploy_contracts.js
+++ project-6/migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
 // migrating the appropriate contracts
-var FarmerRole = artifacts.require("./FarmerRole.sol");
-var DistributorRole = artifacts.require("./DistributorRole.sol");
-var RetailerRole = artifacts.require("./RetailerRole.sol");
-var ConsumerRole = artifacts.require("./ConsumerRole.sol");
-var SupplyChain = artifacts.require("./SupplyChain.sol");
+var FarmerRole = artifacts.require('./FarmerRole.sol');
+var DistributorRole = artifacts.require('./DistributorRole.sol');
+var RetailerRole = artifacts.require('./RetailerRole.sol');
+var ConsumerRole = artifacts.require('./ConsumerRole.sol');
+var SupplyChain = artifacts.require('./SupplyChain.sol');
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
   deployer.deploy(FarmerRole);
   deployer.deploy(DistributorRole);
   deployer.deploy(RetailerRole);
